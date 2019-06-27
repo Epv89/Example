229285c5f41a28ec1629dff8c1fce7763ef9af7b
Epv89 <rr89@inbox.ru>
2019-06-27 23:20:17 +0300

    add Lesson-2.js

---
+++ Lesson-2.js
@@ -0,0 +1,21 @@
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+     console.log(i);
+}
+
+//    let i = 1;
+//    while (i <8) {
+//        console.log (i);
+//        i ++; 
+// }
+
+
+//    do {
+//        console.log(i);
+//        i++;
+// }
+// while (i < 8);
+
